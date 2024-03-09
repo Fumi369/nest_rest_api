3fcd1c2e539002a74984db9a27b4007ec4a7cc7a
Fumi <issaradango@gmail.com>
2024-03-09 20:46:50 +0900

    save

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // this controller handles the "/users" route and its child route(s)
+export class UsersController {
+  /*
+  POST    /users
+  PATCH   /users/:id
+  DELETE  /users/:id
+  */
+
+  @Get() // GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get('interns') // GET users/interns
+  findAllInterns() {
+    // findAll can't be used as a method name here, as it's already in use for another method.
+    return [];
+  }
+
+  @Get(':id') // GET users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+}
