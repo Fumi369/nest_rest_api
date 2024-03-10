afe31a7c1f8da61ab55cae326c3640004d8055ca
Fumi <issaradango@gmail.com>
2024-03-10 16:04:08 +0900

    finish lesson three

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,37 +8,55 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users') // this controller handles the "/users" route and its child route(s)
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or /users?role=value
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
-  @Get('interns') // GET users/interns
-  findAllInterns() {
-    // findAll can't be used as a method name here, as it's already in use for another method.
-    return [];
-  }
+  // @Get('interns') // GET users/interns
+  // findAllInterns() {
+  //   // findAll can't be used as a method name here, as it's already in use for another method.
+  //   return [];
+  // }
 
   @Get(':id') // GET users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
